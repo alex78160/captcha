87d3ed3f4256dc97dcab9ace707f5178f7782cc1
Alexandre Genelle <alexandre.genelle@gmail.com>
2020-04-03 22:07:45 +0200

    externalize tor port

--- login.sh
+++ login.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+torport=9050
+dlfolder=/data/data/com.termux/files/home/storage/downloads/
 echo "enter the username : "
 read -s username
 #echo "enter the password for $username : "
@@ -12,12 +14,23 @@ rootFolder=$(date +%Y%m%d-%H%M%S)
 mkdir $rootFolder
 cd $rootFolder
 
+if nc -z 127.0.0.1 $torport 2>/dev/null
+then
+	echo "tor started, listen port $torport"
+else
+	echo "starting tor"
+	tor &
+	echo "waiting 15s"
+	sleep 15
+	echo "end wait"
+fi
+
 # recup cookie login
-#curl --socks5-hostname 127.0.0.1:9150 "http://apollonxilywevef.onion/login.php" --silent --compressed -c cook1.txt
+#curl --socks5-hostname 127.0.0.1:$torport "http://apollonxilywevef.onion/login.php" --silent --compressed -c cook1.txt
 # recup page login avec le premier cookie
-#curl --socks5-hostname 127.0.0.1:9150 "http://apollonxilywevef.onion/login.php" --silent --compressed --output out1.html -b cook1.txt
+#curl --socks5-hostname 127.0.0.1:$torport "http://apollonxilywevef.onion/login.php" --silent --compressed --output out1.html -b cook1.txt
 # recup img captcha
-#curl --socks5-hostname 127.0.0.1:9150 "http://apollonxilywevef.onion/cap/capshow.png" --silent --compressed --output img.jpg -b cook1.txt
+#curl --socks5-hostname 127.0.0.1:$torport "http://apollonxilywevef.onion/cap/capshow.png" --silent --compressed --output img.jpg -b cook1.txt
 # copie img dans downloads de stockage interne
 #cp img.jpg /data/data/com.termux/files/home/storage/downloads/
 
@@ -36,17 +49,17 @@ cd $rootFolder
 #read capt
 
 # post login avec captcha
-#curl --socks5-hostname 127.0.0.1:9150 "http://apollonxilywevef.onion/login.php" --silent --compressed --output out2.html -b cook1.txt --data "l_username=$username&l_password=*****&capt_code=$capt" -L
+#curl --socks5-hostname 127.0.0.1:$torport "http://apollonxilywevef.onion/login.php" --silent --compressed --output out2.html -b cook1.txt --data "l_username=$username&l_password=*****&capt_code=$capt" -L
 statutLogin=1
 while [ "$statutLogin" -ne "0" ]
 do
-	curl --socks5-hostname 127.0.0.1:9150 "http://apollonxilywevef.onion/login.php" --silent --compressed -c cook1.txt
-	curl --socks5-hostname 127.0.0.1:9150 "http://apollonxilywevef.onion/login.php" --silent --compressed --output out1.html -b cook1.txt
-	curl --socks5-hostname 127.0.0.1:9150 "http://apollonxilywevef.onion/cap/capshow.png" --silent --compressed --output img.jpg -b cook1.txt
-	cp img.jpg /data/data/com.termux/files/home/storage/downloads/
+	curl --socks5-hostname 127.0.0.1:$torport "http://apollonxilywevef.onion/login.php" --silent --compressed -c cook1.txt
+	curl --socks5-hostname 127.0.0.1:$torport "http://apollonxilywevef.onion/login.php" --silent --compressed --output out1.html -b cook1.txt
+	curl --socks5-hostname 127.0.0.1:$torport "http://apollonxilywevef.onion/cap/capshow.png" --silent --compressed --output img.jpg -b cook1.txt
+	cp img.jpg $dlfolder
 	identify img.jpg
 	convert img.jpg -crop 215x65+0+0 img2.png
-	cp img2.png ../../storage/downloads/
+	cp img2.png $dlfolder
 	convert img2.png -colorspace gray -threshold 70% img3.png
 	convert img3.png -resize 350x80! img5.png
 	tesseract --psm 8 --oem 3 img2.png -
@@ -54,7 +67,7 @@ do
 	tesseract --psm 8 --oem 3 img5.png -
 	capt=$(tesseract --psm 8 --oem 3 img5.png -)
 	echo "capt : $capt"
-	curl --socks5-hostname 127.0.0.1:9150 "http://apollonxilywevef.onion/login.php" --silent --compressed --output out2.html -b cook1.txt --data "l_username=$username&l_password=$password&capt_code=$capt" -L
+	curl --socks5-hostname 127.0.0.1:$torport "http://apollonxilywevef.onion/login.php" --silent --compressed --output out2.html -b cook1.txt --data "l_username=$username&l_password=$password&capt_code=$capt" -L
 
 	grep -i camille out2.html
 	statutLogin=$?
